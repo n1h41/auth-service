110f5e4e5a27e79dc6a3b36b2444f6a8c89be854
n1h41 <nihalninu25@gmail.com>
2023-10-28 19:32:47 +0530

    Updated PassResetCodeRequest model to include newPassword and model validation

--- models/pass.reset.code.request.go
+++ models/pass.reset.code.request.go
@@ -1,6 +1,7 @@
 package models
 
 type PassResetCodeRequest struct {
-	UserId     int64 `json:"user_id"`
-	ResetCode string `json:"reset_code"`
+	UserId      int64  `json:"user_id" validate:"required"`
+	ResetCode   string `json:"reset_code" validate:"required"`
+	NewPassword string `json:"new_password" validate:"required"`
 }
